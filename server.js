6c5dc130ee922e8717ff86aba372c2c644f29c6b
SalmaHossain <105491341+SalmaHossain@users.noreply.github.com>
2025-06-30 22:50:41 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const { OAuth2Client } = require('google-auth-library');
-const foodRoutes = require('./foodRoute');
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
-const GOOGLE_CLIENT_ID = '640464062291-fv1cr45pc5hvt61d83q4q6f6hlmee57b.apps.googleusercontent.com';
+import "dotenv/config";
 
+
+// App config
 const app = express();
-const PORT = process.env.PORT || 5000;
-const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+const port = 4000;
 
+// Middleware
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
 
-require('./db');
+//db connection
+connectDB();
 
-app.use('/api/food', foodRoutes); 
+// api endpoints
+app.use("/api/food",foodRouter);
+app.use("/images",express.static('uploads'))
 
-app.post('/api/auth/google', async (req, res) => {
-  const { token } = req.body;
-
-  try {
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: GOOGLE_CLIENT_ID,
-    });
-
-    const payload = ticket.getPayload();
-    const { email, name, picture } = payload;
-
-    res.status(200).json({
-      success: true,
-      user: { email, name, picture }
-    });
-  } catch (error) {
-    console.error("Google login failed:", error);
-    res.status(401).json({ success: false, message: 'Invalid token' });
-  }
+app.get("/", (req, res) => {
+    res.send("API Working");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
 });
+
